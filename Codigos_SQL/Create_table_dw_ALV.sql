b3720473385af5c53d1791cf29933f057c39e08a
TFrankeM <105611304+TFrankeM@users.noreply.github.com>
2023-04-10 22:52:43 -0300

    Criar modelo estrela

--- Codigos_SQL/Create_table_dw_ALV.sql
+++ Codigos_SQL/Create_table_dw_ALV.sql
@@ -1,38 +1,39 @@
-CREATE TABLE Usuario
+CREATE TABLE USUARIO_Dimensao
 (
   UsuarioID INT NOT NULL,
   UsuarioChave INT NOT NULL,
-  UsuarioNome INT NOT NULL,
-  Logradouro INT NOT NULL,
-  Bairro INT NOT NULL,
-  Municipio INT NOT NULL,
-  Estado INT NOT NULL,
+  UsuarioNome VARCHAR(100) NOT NULL,
+  Logradouro VARCHAR(255) NOT NULL,
+  Bairro VARCHAR(100) NOT NULL,
+  Municipio VARCHAR(100) NOT NULL,
+  Estado CHAR(2) NOT NULL,
   PRIMARY KEY (UsuarioChave)
 );
 
-CREATE TABLE Filme
+CREATE TABLE FILME_Dimensao
 (
   FilmeID INT NOT NULL,
   FilmeChave INT NOT NULL,
-  FilmeNome INT NOT NULL,
+  FilmeNome VARCHAR(255) NOT NULL,
   Duracao INT NOT NULL,
-  ValorRoyalty INT NOT NULL,
+  ValorRoyalty money NOT NULL,
   AnoDeLancamento INT NOT NULL,
+  MediaAvaliacao DECIMAL(3,2),
   PRIMARY KEY (FilmeChave)
 );
 
-CREATE TABLE Genero
+CREATE TABLE GENERO_Dimensao
 (
   GeneroID INT NOT NULL,
   GeneroChave INT NOT NULL,
-  GeneroNome INT NOT NULL,
+  GeneroNome VARCHAR(100) NOT NULL,
   PRIMARY KEY (GeneroChave)
 );
 
-CREATE TABLE Calendario
+CREATE TABLE CALENDARIO_Dimensao
 (
   CalendarioChave INT NOT NULL,
-  DataCompleta INT NOT NULL,
+  DataCompleta DATETIME NOT NULL,
   DiaDaSemana INT NOT NULL,
   DiaDoMes INT NOT NULL,
   Mes INT NOT NULL,
@@ -41,28 +42,41 @@ CREATE TABLE Calendario
   PRIMARY KEY (CalendarioChave)
 );
 
-CREATE TABLE Produtora
+CREATE TABLE PRODUTORA_Dimensao
 (
   ProdutoraChave INT NOT NULL,
   ProdutoraID INT NOT NULL,
-  ProdutoraNome INT NOT NULL,
+  ProdutoraNome VARCHAR(255) NOT NULL,
   PRIMARY KEY (ProdutoraChave)
 );
 
-CREATE TABLE Avaliacao
+CREATE TABLE RECEITA
 (
-  AvaliacaoID INT NOT NULL,
+  AssinaturaID INT NOT NULL,
+  ValorPago money NOT NULL,
+  AssinaturaHora DATETIME NOT NULL,
+  CalendarioChave INT NOT NULL,
+  UsuarioChave INT NOT NULL,
+  FilmeChave INT NOT NULL,
+  PRIMARY KEY (AssinaturaID),
+  FOREIGN KEY (CalendarioChave) REFERENCES CALENDARIO_Dimensao(CalendarioChave),
+  FOREIGN KEY (UsuarioChave) REFERENCES USUARIO_Dimensao(UsuarioChave),
+  FOREIGN KEY (FilmeChave) REFERENCES FILME_Dimensao(FilmeChave)
+);
+
+CREATE TABLE AVALIACAO
+(
   AvaliacaoNota INT NOT NULL,
-  AvaliacaoHora INT NOT NULL,
-  CalendárioChave INT NOT NULL,
+  AvaliacaoHora DATETIME NOT NULL,
+  CalendarioChave INT NOT NULL,
   ClienteChave INT NOT NULL,
   FilmeChave INT NOT NULL,
-  GêneroChave INT NOT NULL,
+  GeneroChave INT NOT NULL,
   ProdutoraChave INT NOT NULL,
-  PRIMARY KEY (AvaliacaoID),
-  FOREIGN KEY (CalendárioChave) REFERENCES Calendario(CalendarioChave),
-  FOREIGN KEY (ClienteChave) REFERENCES Usuario(UsuarioChave),
-  FOREIGN KEY (FilmeChave) REFERENCES Filme(FilmeChave),
-  FOREIGN KEY (GêneroChave) REFERENCES Genero(GeneroChave),
-  FOREIGN KEY (ProdutoraChave) REFERENCES Produtora(ProdutoraChave)
+  PRIMARY KEY (CalendarioChave, FilmeChave),
+  FOREIGN KEY (CalendarioChave) REFERENCES CALENDARIO_Dimensao(CalendarioChave),
+  FOREIGN KEY (ClienteChave) REFERENCES USUARIO_Dimensao(UsuarioChave),
+  FOREIGN KEY (FilmeChave) REFERENCES FILME_Dimensao(FilmeChave),
+  FOREIGN KEY (GeneroChave) REFERENCES GENERO_Dimensao(GeneroChave),
+  FOREIGN KEY (ProdutoraChave) REFERENCES PRODUTORA_Dimensao(ProdutoraChave)
 );
