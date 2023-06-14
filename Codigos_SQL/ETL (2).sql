f85648172327f9d6addfaf6a14e1a232e10b44c0
Lucas Hwang Cuan <B46988@fgv.edu.br>
2023-06-13 22:40:22 -0300

    termine o etl

--- Codigos_SQL/ETL (2).sql
+++ Codigos_SQL/ETL (2).sql
@@ -1,33 +1,40 @@
 use DBVLT_ALV_CFB
 go
 
+DECLARE @myguid uniqueidentifier 
+SET @myguid = NEWID()
+
 -- Inserir dados em Produtora
-INSERT INTO [DWVLT_ALV_CFB].[dbo].[PRODUTORA]
+INSERT INTO [DWVLT_ALV_CFB].[dbo].[PRODUTORA] 
 select 
+	newid(),
 	p.ProdutoraID,
     p.ProdutoraNome
 from
 	Produtora p;
 
 -- Inserir dados em Genero
-INSERT INTO [DWVLT_ALV_CFB].[dbo].[GENERO]
+INSERT INTO [DWVLT_ALV_CFB].[dbo].[GENERO] 
 select 
+	newid(),
 	g.GeneroID,
     g.GeneroNome
 from
-	Genero g
+	Genero g;
 
 -- Inserir dados em Filme
-INSERT INTO [DWVLT_ALV_CFB].[dbo].[FILME]
+INSERT INTO [DWVLT_ALV_CFB].[dbo].[FILME] 
 select 
+	newid(),
 	f.FilmeID,
     f.FilmeNome
 from
 	Filme f;
 
 -- Inserir dados em Usuario
-INSERT INTO [DWVLT_ALV_CFB].[dbo].[USUARIO]
+INSERT INTO [DWVLT_ALV_CFB].[dbo].[USUARIO] 
 select 
+	newid(),
 	u.UsuarioID,
     u.UsuarioNome,
     u.Logradouro,
@@ -40,6 +47,7 @@ from
 -- Inserir dados em Calendario
 INSERT INTO [DWVLT_ALV_CFB].[dbo].[CALENDARIO]
 select 
+	newid(),
     a.DataInicio,
 	'AS',
 	DATEPART(WEEKDAY, a.DataInicio),
@@ -53,8 +61,9 @@ select
 from
 	Assinatura a;
 
-INSERT INTO [DWVLT_ALV_CFB].[dbo].[CALENDARIO]
+INSERT INTO [DWVLT_ALV_CFB].[dbo].[CALENDARIO] 
 select 
+	newid(),
     av.AvaliacaoData,
     'AV',
 	DATEPART(WEEKDAY, av.AvaliacaoData),
@@ -68,7 +77,7 @@ select
 from
 	Avaliacao av;
 
-INSERT INTO [DWVLT_ALV_CFB].[dbo].[RECEITA]
+INSERT INTO [DWVLT_ALV_CFB].[dbo].[RECEITA] 
 select
 	c.CalendarioChave,
 	u.UsuarioChave,
@@ -83,27 +92,70 @@ from
 	on a.PlanoID = p.PlanoID;
 	
 
-INSERT INTO [DWVLT_ALV_CFB].[dbo].[AVALIACAO]
+INSERT INTO [DWVLT_ALV_CFB].[dbo].[AVALIACAO] 
 select 
 	u.UsuarioChave,
 	g.GeneroChave,
 	p.ProdutoraChave,
 	c.CalendarioChave,
 	f.FilmeChave,
-	a.AvaliacaoNota
+	a.Nota
 from
-		Avaliacao a 
-	inner join [DWVLT_ALV_CFB].[dbo].[USUARIO] u
-		on a.UsuarioID = u.UsuarioID
+	Avaliacao a inner join [DWVLT_ALV_CFB].[dbo].[USUARIO] u
+	on a.UsuarioID = u.UsuarioID
 	inner join [DWVLT_ALV_CFB].[dbo].[FILME] f
-		on f.FilmeID = a.FilmeID
+	on f.FilmeID = a.FilmeID
 	inner join Produz pr 
-		on f.FilmeID = pr.FilmeID
-	inner join Produtora p
-		on pr.ProdutoraID = p.ProdutoraID 
+	on f.FilmeID = pr.FilmeID
+	inner join [DWVLT_ALV_CFB].[dbo].[PRODUTORA] p
+	on pr.ProdutoraID = p.ProdutoraID 
 	inner join [DWVLT_ALV_CFB].[dbo].[CALENDARIO] c 
-		on a.AvaliacaoData = c.DataCompleta
+	on a.AvaliacaoData = c.DataCompleta
 	inner join Possui po
-		on po.FilmeID = f.FilmeID
-	inner join Genero g
-		on g.GeneroNome = po.GeneroNome
+	on po.FilmeID = f.FilmeID
+	inner join [DWVLT_ALV_CFB].[dbo].[GENERO] g
+	on g.GeneroNome = po.GeneroNome
+
+INSERT INTO [DWVLT_ALV_CFB].[dbo].[CHURN]
+select
+	u.UsuarioChave,
+	c.CalendarioChave
+from
+	Assinatura a inner join [DWVLT_ALV_CFB].[dbo].[USUARIO] u 
+	on a.UsuarioID = u.UsuarioID
+	inner join [DWVLT_ALV_CFB].[dbo].[CALENDARIO] c
+	on a.DataFim = c.DataCompleta
+
+use DWVLT_ALV_CFB
+go
+
+CREATE VIEW VIEW_AVALIACAO AS 
+
+SELECT
+	u.UsuarioNome,
+	f.FilmeNome,
+	p.ProdutoraNome,
+	g.GeneroNome,
+	a.AvaliacaoNota,
+	c.DataCompleta,
+	c.Ano,
+	c.DiaDoMes,
+	c.DiaDaSemana,
+	c.Trimestre,
+	c.Hora,
+	c.Minuto,
+	c.Segundo
+
+FROM 
+
+	Avaliacao a INNER JOIN  CALENDARIO c
+		ON a.CalendarioChave = c.CalendarioChave
+	INNER JOIN Usuario u
+		ON u.UsuarioChave = a.ClienteChave
+	INNER JOIN Filme f 
+		ON f.FilmeChave = a.FilmeChave
+	INNER JOIN Genero g
+		ON g.GeneroChave = a.GeneroChave
+	INNER JOIN PRODUTORA p
+		ON p.ProdutoraChave = a.ProdutoraChave
+	;
